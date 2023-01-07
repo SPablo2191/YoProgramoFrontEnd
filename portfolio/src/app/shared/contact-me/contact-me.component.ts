5f2432e22df625cf593078d6f01f7d8ea335f356
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-01-07 17:17:41 -0300

    🐛 fix: contact me with formGroup

--- portfolio/src/app/shared/contact-me/contact-me.component.ts
+++ portfolio/src/app/shared/contact-me/contact-me.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { abstractForm } from 'src/app/core/classes/abstract-form';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-contact-me',
   templateUrl: './contact-me.component.html',
   styleUrls: ['./contact-me.component.css']
 })
-export class ContactMeComponent implements OnInit {
+export class ContactMeComponent extends abstractForm implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder,messageService : MessageService, userService : UserService) {
+    super(messageService,userService);
+  }
 
   ngOnInit(): void {
+    this.formGroup = this.fb.group({
+      message: []
+    });
   }
 
 }
--- portfolio/src/app/shared/ui-text-area/ui-text-area.component.ts
+++ portfolio/src/app/shared/ui-text-area/ui-text-area.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-ui-text-area',
@@ -7,6 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class UiTextAreaComponent implements OnInit {
   @Input() label! : string;
+  @Input() group!: FormGroup;
+  @Input() name!: string;
   constructor() { }
 
   ngOnInit(): void {
