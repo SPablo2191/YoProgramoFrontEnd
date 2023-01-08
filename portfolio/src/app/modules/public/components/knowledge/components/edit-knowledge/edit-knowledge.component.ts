880bb9707c033e96b875145d4d49d88e5523ba36
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-01-08 02:08:42 -0300

    ✨ fix: editKnowledge in progress

--- portfolio/src/app/modules/public/components/knowledge/components/edit-knowledge/edit-knowledge.component.ts
+++ portfolio/src/app/modules/public/components/knowledge/components/edit-knowledge/edit-knowledge.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { abstractForm } from 'src/app/core/classes/abstract-form';
+import { KnowledgeService } from 'src/app/services/knowledge.service';
 
 @Component({
   selector: 'app-edit-knowledge',
   templateUrl: './edit-knowledge.component.html',
-  styleUrls: ['./edit-knowledge.component.css']
+  styleUrls: ['./edit-knowledge.component.css'],
 })
-export class EditKnowledgeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class EditKnowledgeComponent extends abstractForm implements OnInit {
+  constructor(
+    private fb : FormBuilder,
+    ref: DynamicDialogRef,
+    messageService: MessageService,
+    knowledgeService: KnowledgeService
+  ) {
+    super(ref,messageService,knowledgeService);
   }
 
+  ngOnInit(): void {
+    this.formGroup = this.fb.group({
+      name : [],
+      value : [],
+      color : []
+    })
+  }
 }
