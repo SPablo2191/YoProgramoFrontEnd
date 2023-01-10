089e199b564cb1d35138dd0f47b82e2ccb933950
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-01-10 18:04:05 -0300

    ⚡️ fix: edit and delete in study item

--- portfolio/src/app/shared/ui-study-item/ui-study-item.component.ts
+++ portfolio/src/app/shared/ui-study-item/ui-study-item.component.ts
@@ -1,18 +1,27 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ConfirmationService } from 'primeng/api';
+import { DialogService } from 'primeng/dynamicdialog';
+import { crud } from 'src/app/core/classes/crud.class';
+import { Study } from 'src/app/models/Study.interface';
+import { EditEducationComponent } from 'src/app/modules/public/components/education/components/edit-education/edit-education.component';
+import { StudiesService } from 'src/app/services/studies.service';
 
 @Component({
   selector: 'app-ui-study-item',
   templateUrl: './ui-study-item.component.html',
   styleUrls: ['./ui-study-item.component.css']
 })
-export class UiStudyItemComponent implements OnInit {
-  @Input() title! : string;
-  @Input() image! : string;
-  @Input() description! : string;
-  @Input() startDate! : Date;
-  @Input() endDate! : Date;
-  constructor() { }
-
+export class UiStudyItemComponent extends crud implements OnInit {
+  @Input() item! : Study;
+  override title: string ='Estudio';
+  override editComponent: any = EditEducationComponent;
+  constructor(
+     dialogService: DialogService,
+     api: StudiesService,
+     confirmationService: ConfirmationService
+  ) {
+    super(dialogService,api,confirmationService);
+  }
   ngOnInit(): void {
   }
 
